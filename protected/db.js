adca9f20f5b9b28123f39b0b4758a7d7df2f7d9a
ElevenNie11 <nieshiyi20060927@qq.com>
2026-06-29 00:07:43 +0800

    数据库服务代码复用

---
+++ protected/db.js
@@ -0,0 +1,21 @@
+//导入 mongoose
+const mongoose = require('mongoose');
+
+//连接 mongodb 服务：数据库名称叫做userLogin
+mongoose.connect('mongodb://127.0.0.1:27017/userLogin');
+
+//设置回调
+mongoose.connection.once('open', () => {
+    console.log('连接成功');
+});
+
+mongoose.connection.on('error', () => {
+    console.log('连接失败');
+});
+
+mongoose.connection.once('close', () => {
+    console.log('连接关闭');
+});
+
+//导出 mongoose 实例，实现代码复用
+module.exports = mongoose;
